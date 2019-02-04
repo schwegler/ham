cf134c1dbddb47a7fa41a390cdb184e9c7a7631b
Andrew Schwegler <schwegler@gmail.com>
2019-02-04 16:29:12 -0600

    Finish basic hooking and Giphy init.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { ApiService } from './api.service';
+import { CandidateAddComponent } from './candidate-add/candidate-add.component';
+import { CandidateListComponent } from './candidate-list/candidate-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidateAddComponent,
+    CandidateListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'products',
+        component: CandidateListComponent
+      },
+      {
+        path: 'candidates/add',
+        component: CandidateAddComponent
+      },
+      {
+        path: 'candidate/add/:id',
+        component: CandidateAddComponent
+      },
+    ]),
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphy_api_key: "DfvcZ0w0sqp4S0n6ZAdhi0dgaWUkEzvs"
 };
 
 /*
